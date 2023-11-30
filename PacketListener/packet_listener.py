4afcd116a8f0463c27bbe35fe6fee48136cf6ae5
JGardner1717 <126486805+JGardner1717@users.noreply.github.com>
2023-11-30 01:20:01 -0500

    Add files via upload

--- PacketListener/packet_listener.py
+++ PacketListener/packet_listener.py
@@ -1,20 +1,19 @@
 import scapy.all as scapy
-#from scapy_http import http
 
 def listen_packets(interface):
 
     scapy.sniff(iface=interface,store=False,prn=analyze_packets)
-    #prn = callback function
 
 def analyze_packets(packet):
-    #packet.show()
     if packet.haslayer(scapy.TCP) and packet.haslayer(scapy.Raw):
-        # Attempt to decode HTTP content from the Raw layer
+        # Decode HTTP content from the Raw layer
         try:
             http_payload = packet[scapy.Raw].load.decode('utf-8')
-            if "HTTP" in http_payload:  # Simple check for HTTP content
+            # Printing http packets
+            if "HTTP" in http_payload:  
                 print(http_payload)
         except UnicodeDecodeError:
-            pass  # Ignore decode errors
+            # Ignorng Decode Errors
+            pass  
 
 listen_packets("eth0")
